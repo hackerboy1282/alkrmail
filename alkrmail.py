b84ca9a828be54d8ffe334d2204cc5298a501f0a
hackerboy1282 <54165483+hackerboy1282@users.noreply.github.com>
2021-02-10 16:22:49 +0530

    Update alkrmail.py

--- alkrmail.py
+++ alkrmail.py
@@ -5,7 +5,7 @@ import smtplib
 from os import system
 
 def main():
-   print """
+   print
   ____                 _ _ _   _            _
  / __|_ __ ___   __ _(_) | | | | __ _  ___| | __
 | ( | |  _   _ \ / _` | | | |_| |/ _` |/ __| |/ /
